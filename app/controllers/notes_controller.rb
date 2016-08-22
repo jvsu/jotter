b825c1010ba62c8973a6db376441bc53588d600c
Joseph Su <josephsu@Josephs-MacBook-Air.local>
2016-08-22 15:51:15 -0700

    updated notes controller method new and update

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,32 +6,27 @@ class NotesController < ApplicationController
   end
 
   def create
-    time_h = note_params[:start_timeh]
-    time1 = note_params[:start_time1]
-    time2 = note_params[:start_time2]
-    # if time_h.is_a?(Integer) && time1.is_a?(Integer)  && time2.is_a?(Integer) # need to write a way to handle non number inputs
+    #Check if time stamp is a number and if so convert it to youtube time stamp
+     time_info =  Note.time_stamp(note_params[:start_timeh],note_params[:start_time1],note_params[:start_time2])
 
-      #need to convert time to seconds/integer
-      time_h_int = time_h.to_i
-      time1_int = time1.to_i 
-      time2_int = time2.to_i
-      time_stamp = (time_h_int *3600) + (time1_int *60)+ time2_int
+    if time_info !=false
+      #create new note
 
-      note = Note.new(url:note_params[:url], user_id:note_params[:user_id],start_time:time_stamp, time_stamp_h:time_h_int, time_stamp_m:time1_int,time_stamp_s:time2_int, notes:note_params[:notes], collection_id:note_params[:collection_id])
-      if note.save
-        json_data = {success:'true',time:time_stamp, note_id:note.id,start_timeh:note_params[:start_timeh] ,url:note_params[:url], user_id:note_params[:user_id],start_time1:note_params[:start_time1], start_time2:note_params[:start_time2], notes:note_params[:notes], collection_id:note_params[:collection_id]}
-        render json:json_data
-
-      else
-         messages = note.errors.full_messages
-          json_data = {success:'false', messages:messages}
-         render json:json_data
-      end
-    # else
-    #    json_data = {success:'false', messages:"Time Entered Needs to be a Number"}
-    #    render json:json_data
-    # end
+      note = Note.new(url:note_params[:url], user_id:note_params[:user_id],start_time:time_info[:time_stamp], time_stamp_h:time_info[:time_h], time_stamp_m:time_info[:time_m],time_stamp_s:time_info[:time_s], notes:note_params[:notes], collection_id:note_params[:collection_id])
+        if note.save
+          json_data = {success:'true',time:time_info[:time_stamp], note_id:note.id,start_timeh:note_params[:start_timeh] ,url:note_params[:url], user_id:note_params[:user_id],start_time1:note_params[:start_time1], start_time2:note_params[:start_time2], notes:note_params[:notes], collection_id:note_params[:collection_id]}
+          render json:json_data
+        else
+           messages = note.errors.full_messages
+            json_data = {success:'false', messages:messages}
+           render json:json_data
+        end
+    else
+       json_data = {success:'false', messages:"Time Entered Needs to be a Number"}
+       render json:json_data
+    end
   end 
+
 
   def retrieve
     note_id = params[:notes_id]
@@ -40,24 +35,24 @@ class NotesController < ApplicationController
 
   def update
     if note_update[:source] =='time'
-      time_h = note_update[:start_timeh]
-      time1 = note_update[:start_time1]
-      time2 = note_update[:start_time2]
-      time_h_int = time_h.to_i
-      time1_int = time1.to_i 
-      time2_int = time2.to_i
-      time_stamp = (time_h_int *3600) + (time1_int *60)+ time2_int
-      @note = Note.find(note_update[:note_id]).update(start_time:time_stamp, time_stamp_h:time_h_int, time_stamp_m:time1_int,time_stamp_s:time2_int)
-       json_data = {message:'success',time_h:time_h_int,time_m:time1_int, time_s:time2_int}
-      render json:json_data
+        time_info = Note.time_stamp(note_update[:start_timeh],note_update[:start_time1],note_update[:start_time2])
 
+        # if a number
+        if time_info != false
+          @note = Note.find(note_update[:note_id]).update(start_time:time_info[:time_stamp], time_stamp_h:time_info[:time_h], time_stamp_m:time_info[:time_m],time_stamp_s:time_info[:time_s])
+          json_data = {message:'success',time_h:time_info[:time_h],time_m:time_info[:time_m], time_s:time_info[:time_s]}
+          render json:json_data
+        else
+          json_data = {success:'false', messages:"Time Entered Needs to be a Number"}
+          render json:json_data
+        end
+     
+      #if not a number
     elsif note_update[:source]=='note'
         @note = Note.find(note_update[:note_id]).update(:notes=>note_update[:notes])
         note = note_update[:notes]
         json_data = {message:'success',notes:note}
         render json:json_data
-    else
-
     end
   
   end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,41 @@
 class Note < ActiveRecord::Base
-    belongs_to :user
+  belongs_to :user
   belongs_to :collection
   has_many :group_notes
   has_many :point_supports
   validates :notes, :url, presence:true
   validates :user_id, :start_time,:time_stamp_h, :collection_id, :time_stamp_m, :time_stamp_s, presence:true, numericality:true
+
+
+  def self.valid_float(obj)
+    obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+  end
+
+  #generate time stamp
+  def self.time_stamp(start_timeh,start_time1, start_time2) # the hour, minute and second parameters
+	#check if time inputs are integers
+	time1 = valid_float(start_timeh)
+	time2 = valid_float(start_time1)
+	time3 = valid_float(start_time2)
+
+	if time1 ==true && time2==true && time3==true
+	 #assign to arguments to parameters
+		time_h = start_timeh
+	    time1 = start_time1
+	    time2 = start_time2
+	 #need to convert time to seconds/integer
+	      time_h_int = time_h.to_i
+	      time1_int = time1.to_i 
+	      time2_int = time2.to_i
+	      time_stamp = (time_h_int *3600) + (time1_int *60)+ time2_int
+	  		time_info ={time_stamp:time_stamp, time_h:time_h_int, time_m:time1_int,time_s:time2_int}
+		 return time_info
+	else
+		return false
+	end
+end
+
+
+
+
 end
