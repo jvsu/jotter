02325b653c6146f9f8ce6b4dfdd77eab90f70687
Joseph Su <josephsu@Josephs-MacBook-Air.local>
2016-08-22 20:48:42 -0700

    refactor your_tags controller

--- app/controllers/your_tags_controller.rb
+++ app/controllers/your_tags_controller.rb
@@ -4,39 +4,12 @@ class YourTagsController < ApplicationController
     user = session[:user_id]
     @user = User.find(user)
 
-    #get all tag entries
-    tags = Tag.all
-
-    #put all tag_names into an array
-    @tags = []
-
-    tags.each do |t|
-      @tags.push(t.tag_name)
-    end
-
-    #remove duplicates in an array
-    @tags = @tags.uniq
-
+    # get tags for the view
+    tags = YourTag.tags(user)
    
+    @my_tags = tags[:my_tags]
+    @tags = tags[:tags]
 
-    #Get my_tags... tags that belong to you
-
-    mtags = YourTag.all.where(user_id:user)
-    
-    #put it into an array and remove duplicates
-    @my_tags = []
-    
-    mtags.each do |mt|
-      @my_tags.push(mt.tag)
-    end
-
-    @my_tags= @my_tags.uniq
-
-    #go through my_tags array and use array.delete 
-
-    @my_tags.each do |mt|
-        @tags.delete(mt)
-    end
   end
 
   def create
--- app/models/your_tag.rb
+++ app/models/your_tag.rb
@@ -1,3 +1,43 @@
 class YourTag < ActiveRecord::Base
   belongs_to :user
+
+
+ def self.tags(user_id)
+ 	 #get all tag entries
+    tags = Tag.all
+
+    #put all tag_names into an array
+    tags_array = []
+
+    tags.each do |t|
+      tags_array.push(t.tag_name)
+    end
+
+    #remove duplicates in an array
+    tags_array = tags_array.uniq
+
+  
+    #Get my_tags... tags that belong to you
+
+    mtags = YourTag.all.where(user_id:user_id)
+    
+    #put it into an array and remove duplicates
+    my_tags_array = []
+    
+    mtags.each do |mt|
+      my_tags_array.push(mt.tag)
+    end
+
+    my_tags_array = my_tags_array.uniq
+
+    #go through my_tags array and use array.delete 
+
+    my_tags_array.each do |mt|
+        tags_array.delete(mt)
+    end
+
+    return tag_data = {tags:tags_array, my_tags:my_tags_array}
+
+ end
+
 end
